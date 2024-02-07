a1f712e3f7fac23a62b851659193bacb0f79bd1f
FA <fa.sadicon@gmail.com>
2024-02-07 08:44:39 +0800

    Form Handling

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -6,21 +6,84 @@ export default {
   name: "App",
   data() {
     return {
-      names: ['Frans', 'Audrey', 'Segovia', 'Sadicon'],
-      fullNames: [
-        { first: "Frans", last: "Sadicon" },
-        { first: "Carl", last: "Labado" },
-        { first: "Jolina", last: "Sants" },
-      ],
-    };
+      formValues: {
+        name: "",
+        profile: "",
+        country: "",
+        jobLocation: [],
+        remoteWork: "no",
+        skillSet: [],
+        yearsOfExperience: ''
+      }
+    }
   },
+  methods: {
+    submitForm (event) {
+      event.preventDefault()
+      console.log(this.formValues)
+    }
+  }
 };
 </script>
 
 <template>
-  <ul>
-    <li v-for="name in fullNames">{{ name.first }} {{ name.last }}</li>
-  </ul>
+  <pre>
+    {{ formValues }}
+  </pre>
+  <form @submit="submitForm">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="formValues.name">
+    </div>
+    <div>
+      <label for="profile">Profle</label>
+      <textarea type="text" id="profile" v-model="formValues.profile" />
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="philippines">Philippines</option>
+        <option value="singapore">Singapore</option>
+        <option value="canada">Canada</option>
+      </select>
+    </div>
+    <div>
+      <label for="jobLocation">Job Location</label>
+      <select id="jobLocation" multiple v-model="formValues.jobLocation">
+        <option value="philippines">Philippines</option>
+        <option value="singapore">Singapore</option>
+        <option value="canada">Canada</option>
+      </select>
+    </div>
+    <div>
+      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+    <div id="skillSets">
+      <label for="skillSets">Skill Sets</label>
+      <input type="checkbox" id="HTML" value="html" v-model="formValues.skillSet">
+      <label for="HTML">HTML</label>
+      <input type="checkbox" id="CSS" value="css" v-model="formValues.skillSet">
+      <label for="CSS">CSS</label>
+      <input type="checkbox" id="JS" value="js" v-model="formValues.skillSet">
+      <label for="JS">JS</label>
+    </div>
+    <div id="yearsOfExperience">
+      <label for="yearsOfExperience">Years of Experience</label>
+      <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
+      <label for="0-2">0 - 2</label>
+      <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
+      <label for="3-5">3 - 5</label>
+      <input type="radio" id="6-8" value="6-8" v-model="formValues.yearsOfExperience">
+      <label for="6-8">6 - 8</label>
+
+    </div>
+
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
 </template>
 
 <!-- <style scoped>
