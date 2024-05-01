58a6d97a7f752d38b094a005240889d1fbb33fde
fassadicon <fa.sadicon@gmail.com>
2024-05-01 16:52:26 +0800

    43 - teleport component
    See:
    
    https://codesandbox.io/s/vishwas-vue-teleport-4z93t

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <Topbar :name />
-  <button @click="activeTab = 'TabA'">Tab A</button>
-  <button @click="activeTab = 'TabB'">Tab B</button>
-  <button @click="activeTab = 'TabC'">Tab C</button>
-
-  <keep-alive>
-    <component :is="activeTab"/>
-  </keep-alive>
-  <!-- <TabA v-if="activeTab === 'TabA'"></TabA>
-  <TabB v-if="activeTab === 'TabB'"></TabB>
-  <TabC v-if="activeTab === 'TabC'"></TabC> -->
+  <teleport to="#portal-root">
+    <Portal/>
+  </teleport>
 </template>
 
 <script>
 import Topbar from "./layouts/Topbar.vue";
-import TabA from "./components/TabA.vue";
-import TabB from "./components/TabB.vue";
-import TabC from "./components/TabC.vue";
+import Portal from "./components/Portal.vue"
 
 export default {
   name: "App",
   components: {
     Topbar,
-    TabA,
-    TabB,
-    TabC
+    Portal
   },
   data() {
     return {
---
+++ basics/src/components/Portal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Portal</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Portal",
+}
+</script>
+
+<style scoped>
+</style>
